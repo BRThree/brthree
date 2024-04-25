9dfd3e740af880a9dedf5ee9c9ed6a1a8c66d860
BRThree <brthree@dingtalk.com>
2024-04-25 09:52:27 +0800

    feat: live2d模型保存

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 // 要留一个APP入口组件，方便抽离组件的公共代码，不在main.tsx中堆逻辑
 import Router from "@/router/Router.tsx";
+import Live2D from "@/components/Live2D/Live2D.tsx";
 
  const App = () => {
    return (
-     <Router/>
+     <>
+       <Router/>
+       <Live2D/>
+     </>
   )
 }
 
